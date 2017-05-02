60b1f4736d6d929803b4e354caa7dcee95acd993
Steve <drewypnl@hotmail.com>
2017-05-02 20:59:27 +0100

    Complete

--- cookie.php
+++ cookie.php
@@ -5,3 +5,19 @@
  * Date: 02/05/2017
  * Time: 20:46
  */
+if (isset($_GET['cookiecheck']))
+{
+    if (isset($_COOKIE['testcookie']))
+    {
+        print "Cookies are enabled";
+    }
+    else
+    {
+        print "Cookies are no enabled";
+    }
+}
+else
+{
+    setcookie('testcookie', "testvalue");
+    die(header("Location: ".$_SERVER['PHP_SELF']."?cookiecheck=1"));
+}
